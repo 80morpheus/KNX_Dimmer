da1439bf1ecd00f5897a370f903c1ef9da65d02a
80morpheus <45001305+80morpheus@users.noreply.github.com>
2024-09-10 08:51:27 +0200

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <knx.h>
+#include <dimmer.h>
+int ch_num = 0;
+Dimmer dimmer = new Dimmer();
+
+
+
+void setup() 
+{
+    knx.readMemory();
+    if ( knx.configured() )
+    {
+      dimmer.init();
+      ch_num = knx.Parameter()
+      dimmmer.setupCallback();
+    }
+    else
+    {
+          knx.progMode(on);
+    }
+  
+}
+
+
+
+void loop()
+{
+    knx.loop();
+    if ( knx.configured() )
+    {
+          dimmer.loop();
+    }
+
+}
