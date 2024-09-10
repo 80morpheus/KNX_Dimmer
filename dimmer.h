5dd255a1fa9f409e1a080f100094d9e020154ca2
80morpheus <45001305+80morpheus@users.noreply.github.com>
2024-09-10 08:56:13 +0200

    Create dimmer.h

---
+++ dimmer.h
@@ -0,0 +1,12 @@
+#include dimmer.cpp
+
+#define go_ch1_switch
+#define go_ch1_reldim
+#define go_ch1_absdim
+#define go_ch1_color
+#define go_ch1_state
+#define go_ch1_dimmer
+
+#define go_ch2_ 
+....
+
