8225e76a38b4746b92291b9480a5edc5059ab469
80morpheus <45001305+80morpheus@users.noreply.github.com>
2024-10-20 20:34:56 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,8 +1,15 @@
 #include <Arduino.h>
+#define MASK_VERSION 0x07B0
+#define USE_RP2040_EEPROM_EMULATION
 #include <knx.h>
+//#include <Wire.h>
 #include "dimmer.h"
 int ch_num = 0;
 Dimmer dimmer(2);
+
+
+
+
 
 
 
@@ -11,12 +18,15 @@ void setup()
     knx.readMemory();
     if ( knx.configured() )
     {
+      //Wire.begin();
       dimmer.init();
       for ( int i=1; i<17; i++)
       {
-              if ( knx.paramInt(i) > 0 )   {ch_num++ ;  }
+              if ( knx.paramInt(i*10) > 0 )   {ch_num++ ;  }
       }
       dimmer.knxParam(ch_num);
+
+      dimmer.setGroupObject();
       dimmer.setupCallback();
     }
     else
